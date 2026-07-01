620a34852c46783921a4974a06430d85b6ebce96
Anirudh10102003 <anirudhkuila003@gmail.com>
2026-07-01 19:24:28 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const cors = require('cors')
 // console.log("Hello")
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'code-pulse-ebon.vercel.app',
     credentials: true 
 }))
 
